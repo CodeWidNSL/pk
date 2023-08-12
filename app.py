4936e7d573f0bd6369316417cdb47c2aab66c02b
CodeWidNSL <git config --global user.email>
2023-08-12 10:19:45 +0530

    app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/pk_birthday')
+def pk_birthday():
+    return render_template('index.html')
+    # return jsonify({"result" : "successful"})
+
+if __name__ == "__main__":
+    app.run()
